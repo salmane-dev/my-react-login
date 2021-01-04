90ce8f1ccf58828b7565cbfe299a624d97d2b3b7
salmane-dev <49452590+salmane-dev@users.noreply.github.com>
2021-01-04 10:31:25 +0100

    Update Home.js

--- frontend-src/components/Home.js
+++ frontend-src/components/Home.js
@@ -2,14 +2,14 @@ import React, { useEffect } from "react"
 
 function Home() {
   useEffect(() => {
-    document.title = "Our Amazing App"
+    document.title = "My-React-Login"
   }, [])
 
   return (
     <div>
       <h2 className="display-4">Welcome to the homepage!</h2>
       <p className="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque quasi natus fugit ab laudantium dolores, dicta blanditiis sit eum perferendis minima sunt repudiandae voluptates tempore. Fuga dicta ipsa beatae vel?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error eveniet dignissimos alias repellat officia qui rem nemo, unde magnam voluptatum obcaecati explicabo tempora quidem? Aperiam doloremque assumenda accusantium deleniti soluta?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.   </p>
     </div>
   )
 }
