b8d6234283374ded044a6ec41891b0896a8bc340
salmane-dev <49452590+salmane-dev@users.noreply.github.com>
2021-01-04 10:33:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,31 +11,16 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, "public")))
 
-app.get("/api/animals/meowsalot", cors(), (req, res) => {
-  // tell browser that if it loaded this url in the last 10 seconds to use that if quickly switching between pages
-  res.header("Cache-Control", "max-age=10")
-  res.json({ name: "Meowsalot", species: "cat", "photo": "https://learnwebcode.github.io/json-example/images/cat-1.jpg", bio: "This cat is great and very vocal. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis asperiores, sunt consectetur a amet dolorem rem animi tempore molestias nesciunt fuga, sequi alias voluptatum totam reprehenderit assumenda deleniti distinctio? Cumque." })
-})
-
-app.get("/api/animals/barksalot", cors(), (req, res) => {
-  res.header("Cache-Control", "max-age=10")
-  res.json({ name: "Barksalot", species: "dog", "photo": "https://learnwebcode.github.io/json-example/images/dog-1.jpg", bio: "This dog is very communicative. Deleniti, tempora quis commodi qui inventore ratione rem porro doloribus et obcaecati cumque quibusdam voluptatibus iure nisi aut minima consequuntur, officiis esse? Lorem ipsum, dolor sit amet consectetur adipisicing elit." })
-})
-
-app.get("/api/animals/purrsloud", cors(), (req, res) => {
-  res.header("Cache-Control", "max-age=10")
-  res.json({ name: "Purrsloud", species: "cat", "photo": "https://learnwebcode.github.io/json-example/images/cat-2.jpg", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis asperiores, sunt consectetur a amet dolorem rem animi tempore molestias nesciunt fuga, sequi alias voluptatum totam reprehenderit assumenda deleniti distinctio? Cumque. Lorem ipsum." })
-})
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
 app.post("/secret", (req, res) => {
-  if (req.body.username === "johndoe" && req.body.password == "qwerty") {
-    res.json({ message: "The sky is blue.", status: "success" })
+  if (req.body.username === "user44" && req.body.password == "AZERTY") {
+    res.json({ message: " user44 is logged in.", status: "success" })
   } else {
-    res.json({ message: "You are not authorized to access this data.", status: "failure" })
+    res.json({ message: "Something went wrong.", status: "failure" })
   }
 })
 
